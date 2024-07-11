f95f96fe13d1fbe4f01508a3ff6d44bd7975f01e
Gabriel <me@gabrielbull.com>
2024-07-10 22:15:57 -0400

    Initial commit

---
+++ project/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { hello } from "lib";
+
+hello();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
